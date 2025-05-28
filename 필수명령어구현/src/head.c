8911f12cb15ade225f2f92b456b6f9b24072df4c
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 16:47:16 +0900

    Update head.c

--- 필수명령어구현/src/head.c
+++ 필수명령어구현/src/head.c
@@ -1,10 +1,43 @@
-// 19. head.c
 #include <stdio.h>
-// 파일 처음 10줄 출력
+#include <stdlib.h>
+#include <getopt.h>
+
+// head 명령어: 파일의 처음 N줄 출력
+// 옵션: -n <줄 수> (기본 10줄)
 int main(int argc, char *argv[]) {
-    if (argc != 2) { printf("Usage: %s <file>\n", argv[0]); return 1; }
-    FILE *fp = fopen(argv[1], "r"); if (!fp) { perror("fopen"); return 1; }
-    char line[1024]; int count = 0;
-    while (fgets(line, sizeof(line), fp) && count++ < 10) printf("%s", line);
-    fclose(fp); return 0;
+    int opt;
+    int num_lines = 10;
+
+    // 옵션 파싱
+    while ((opt = getopt(argc, argv, "n:")) != -1) {
+        switch (opt) {
+            case 'n':
+                num_lines = atoi(optarg);
+                if (num_lines <= 0) num_lines = 10;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-n num] file\n", argv[0]);
+                return 1;
+        }
+    }
+
+    if (optind >= argc) {
+        fprintf(stderr, "Usage: %s [-n num] file\n", argv[0]);
+        return 1;
+    }
+
+    FILE *fp = fopen(argv[optind], "r");
+    if (!fp) {
+        perror("fopen");
+        return 1;
+    }
+
+    char line[1024];
+    int count = 0;
+    while (fgets(line, sizeof(line), fp) && count++ < num_lines) {
+        printf("%s", line);
+    }
+
+    fclose(fp);
+    return 0;
 }
