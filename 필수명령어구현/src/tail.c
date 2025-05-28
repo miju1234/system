e7e9bbe31ea7540676358f0193847f5167b5c515
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 16:59:38 +0900

    Update tail.c

--- 필수명령어구현/src/tail.c
+++ 필수명령어구현/src/tail.c
@@ -1,13 +1,53 @@
-// 20. tail.c
 #include <stdio.h>
 #include <stdlib.h>
-// 파일 마지막 10줄 출력
+#include <string.h>
+#include <getopt.h>
+
+// tail 명령어: 파일 마지막 N줄 출력
+// 옵션: -n <줄 수> (기본 10줄)
 int main(int argc, char *argv[]) {
-    if (argc != 2) { printf("Usage: %s <file>\n", argv[0]); return 1; }
-    FILE *fp = fopen(argv[1], "r"); if (!fp) { perror("fopen"); return 1; }
-    char *lines[10]; for (int i = 0; i < 10; i++) lines[i] = malloc(1024);
-    int count = 0; while (fgets(lines[count % 10], 1024, fp)) count++;
-    int start = count > 10 ? count % 10 : 0, n = count > 10 ? 10 : count;
-    for (int i = 0; i < n; i++) printf("%s", lines[(start + i) % 10]);
-    fclose(fp); for (int i = 0; i < 10; i++) free(lines[i]); return 0;
+    int opt;
+    int lines = 10;
+
+    while ((opt = getopt(argc, argv, "n:")) != -1) {
+        switch (opt) {
+            case 'n':
+                lines = atoi(optarg);
+                if (lines <= 0) lines = 10;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-n lines] file\n", argv[0]);
+                return 1;
+        }
+    }
+
+    if (optind >= argc) {
+        fprintf(stderr, "Usage: %s [-n lines] file\n", argv[0]);
+        return 1;
+    }
+
+    FILE *fp = fopen(argv[optind], "r");
+    if (!fp) {
+        perror("fopen");
+        return 1;
+    }
+
+    char **buffer = malloc(lines * sizeof(char *));
+    for (int i = 0; i < lines; i++) buffer[i] = malloc(1024);
+    int count = 0;
+
+    while (fgets(buffer[count % lines], 1024, fp)) count++;
+
+    int start = (count >= lines) ? count % lines : 0;
+    int n = (count >= lines) ? lines : count;
+
+    for (int i = 0; i < n; i++) {
+        printf("%s", buffer[(start + i) % lines]);
+    }
+
+    for (int i = 0; i < lines; i++) free(buffer[i]);
+    free(buffer);
+    fclose(fp);
+
+    return 0;
 }
