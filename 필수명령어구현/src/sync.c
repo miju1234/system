6c5ff1619ccda7020468a7cc282e439e8d8cece3
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 16:59:07 +0900

    Update sync.c

--- 필수명령어구현/src/sync.c
+++ 필수명령어구현/src/sync.c
@@ -1,9 +1,18 @@
-// 47. sync.c
 #include <stdio.h>
 #include <unistd.h>
-// 디스크 동기화
-int main() {
+#include <getopt.h>
+
+// sync 명령어: 디스크 버퍼를 동기화
+// 옵션: 없음 (기본 동작만 수행)
+int main(int argc, char *argv[]) {
+    int opt;
+
+    while ((opt = getopt(argc, argv, "")) != -1) {
+        fprintf(stderr, "Usage: %s\n", argv[0]);
+        return 1;
+    }
+
     sync();
-    printf("Disk synchronized.\n");
+    printf("Disk buffers synced.\n");
     return 0;
 }
