7a44cd295ef9b491f9945db822a8a978844a9923
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 16:56:36 +0900

    Update sleep.c

--- 필수명령어구현/src/sleep.c
+++ 필수명령어구현/src/sleep.c
@@ -1,9 +1,31 @@
-// 46. sleep.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-// 지정 시간 대기
-int main(int argc,char *argv[]) {
-    if(argc!=2) { printf("Usage: %s <seconds>\n", argv[0]); return 1; }
-    sleep(atoi(argv[1])); return 0;
+#include <getopt.h>
+
+// sleep 명령어: 지정 시간 동안 대기
+// 옵션: -s <seconds> (대기 시간 초 단위), -m <milliseconds> (대기 시간 밀리초 단위)
+int main(int argc, char *argv[]) {
+    int opt;
+    int seconds = 0;
+    int milliseconds = 0;
+
+    while ((opt = getopt(argc, argv, "s:m:")) != -1) {
+        switch (opt) {
+            case 's':
+                seconds = atoi(optarg);
+                break;
+            case 'm':
+                milliseconds = atoi(optarg);
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-s seconds] [-m milliseconds]\n", argv[0]);
+                return 1;
+        }
+    }
+
+    if (seconds > 0) sleep(seconds);
+    if (milliseconds > 0) usleep(milliseconds * 1000);
+
+    return 0;
 }
