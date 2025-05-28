65f81a3da9612e01b0f36e0819dfb6c42a2702ac
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 16:41:57 +0900

    Update dirname.c

--- 필수명령어구현/src/dirname.c
+++ 필수명령어구현/src/dirname.c
@@ -1,8 +1,38 @@
-// 16. dirname.c
 #include <stdio.h>
 #include <libgen.h>
-// 경로에서 디렉토리 추출
+#include <string.h>
+#include <getopt.h>
+
+// dirname 명령어: 경로에서 디렉토리 추출
+// 옵션: -z (출력을 널 문자로 종료)
 int main(int argc, char *argv[]) {
-    if (argc != 2) { printf("Usage: %s <path>\n", argv[0]); return 1; }
-    printf("%s\n", dirname(argv[1])); return 0;
+    int opt;
+    int zero_terminated = 0;
+
+    // 옵션 파싱
+    while ((opt = getopt(argc, argv, "z")) != -1) {
+        switch (opt) {
+            case 'z':
+                zero_terminated = 1;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-z] path\n", argv[0]);
+                return 1;
+        }
+    }
+
+    if (optind >= argc) {
+        fprintf(stderr, "Usage: %s [-z] path\n", argv[0]);
+        return 1;
+    }
+
+    char *dir = dirname(argv[optind]);
+
+    if (zero_terminated) {
+        printf("%s\0", dir);
+    } else {
+        printf("%s\n", dir);
+    }
+
+    return 0;
 }
