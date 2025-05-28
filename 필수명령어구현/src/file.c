f31d24e64241f7e2096951cb8c167ee7bb205839
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 16:44:46 +0900

    Update file.c

--- 필수명령어구현/src/file.c
+++ 필수명령어구현/src/file.c
@@ -1,13 +1,59 @@
-// 17. file.c
 #include <stdio.h>
 #include <string.h>
 #include <magic.h>
-// 파일 유형 판별 (매직넘버 검사 추가)
+#include <getopt.h>
+
+// file 명령어: 파일 유형 판별 (libmagic 사용)
+// 옵션: -b (간단 출력), -z (압축 파일 처리 허용)
 int main(int argc, char *argv[]) {
-    if (argc != 2) { printf("Usage: %s <file>\n", argv[0]); return 1; }
-    const char *filename = argv[1]; magic_t magic = magic_open(MAGIC_NONE);
-    if (!magic) { fprintf(stderr, "Unable to initialize magic\n"); return 1; }
-    magic_load(magic, NULL); const char *desc = magic_file(magic, filename);
-    if (desc) printf("%s: %s\n", filename, desc); else fprintf(stderr, "Unable to determine type\n");
-    magic_close(magic); return 0;
+    int opt;
+    int brief = 0;
+    int compressed = 0;
+
+    // 옵션 파싱
+    while ((opt = getopt(argc, argv, "bz")) != -1) {
+        switch (opt) {
+            case 'b':
+                brief = 1;
+                break;
+            case 'z':
+                compressed = 1;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-b] [-z] file\n", argv[0]);
+                return 1;
+        }
+    }
+
+    if (optind >= argc) {
+        fprintf(stderr, "Usage: %s [-b] [-z] file\n", argv[0]);
+        return 1;
+    }
+
+    const char *filename = argv[optind];
+    magic_t magic = magic_open(MAGIC_NONE | (compressed ? MAGIC_COMPRESS : 0));
+    if (!magic) {
+        fprintf(stderr, "Unable to initialize magic library\n");
+        return 1;
+    }
+
+    if (magic_load(magic, NULL) != 0) {
+        fprintf(stderr, "Cannot load magic database: %s\n", magic_error(magic));
+        magic_close(magic);
+        return 1;
+    }
+
+    const char *desc = magic_file(magic, filename);
+    if (desc) {
+        if (brief) {
+            printf("%s\n", desc);
+        } else {
+            printf("%s: %s\n", filename, desc);
+        }
+    } else {
+        fprintf(stderr, "Unable to determine file type for %s\n", filename);
+    }
+
+    magic_close(magic);
+    return 0;
 }
