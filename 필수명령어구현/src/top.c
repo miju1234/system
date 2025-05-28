779ae71d3eedbf2715d7403529cc8436b91910bd
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 17:00:39 +0900

    Update top.c

--- 필수명령어구현/src/top.c
+++ 필수명령어구현/src/top.c
@@ -1,8 +1,29 @@
-// 30. top.c
 #include <stdio.h>
 #include <unistd.h>
-// 시스템 모니터링 (간단)
-int main() {
-    for (int i=0; i<5; i++) { printf("Monitoring... %d\n", i+1); sleep(1); }
+#include <getopt.h>
+
+// top 명령어: 시스템 상태 모니터링 (간단)
+// 옵션: -n <횟수> (업데이트 반복 횟수)
+int main(int argc, char *argv[]) {
+    int opt;
+    int iterations = 5;
+
+    while ((opt = getopt(argc, argv, "n:")) != -1) {
+        switch (opt) {
+            case 'n':
+                iterations = atoi(optarg);
+                if (iterations <= 0) iterations = 5;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-n iterations]\n", argv[0]);
+                return 1;
+        }
+    }
+
+    for (int i = 0; i < iterations; i++) {
+        printf("Monitoring system... (iteration %d)\n", i + 1);
+        sleep(1);
+    }
+
     return 0;
 }
