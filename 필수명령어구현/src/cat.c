583b7477ce5687fd74618c1fa75c762caa18dcab
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 16:37:23 +0900

    Update cat.c

--- 필수명령어구현/src/cat.c
+++ 필수명령어구현/src/cat.c
@@ -1,8 +1,61 @@
-// 5. cat.c
 #include <stdio.h>
-// 파일 내용 출력
+#include <stdlib.h>
+#include <getopt.h>
+
+// cat 명령어: 파일 내용 출력
+// 옵션: -n (행 번호 출력), -E (행 끝에 $ 추가)
 int main(int argc, char *argv[]) {
-    if (argc != 2) { printf("Usage: %s <file>\n", argv[0]); return 1; }
-    FILE *fp = fopen(argv[1], "r"); if (!fp) { perror("fopen"); return 1; }
-    int c; while ((c = fgetc(fp)) != EOF) putchar(c); fclose(fp); return 0;
+    int opt;
+    int show_line_numbers = 0;
+    int show_ends = 0;
+
+    // 옵션 파싱
+    while ((opt = getopt(argc, argv, "nE")) != -1) {
+        switch (opt) {
+            case 'n':
+                show_line_numbers = 1;
+                break;
+            case 'E':
+                show_ends = 1;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-n] [-E] [file...]\n", argv[0]);
+                return 1;
+        }
+    }
+
+    if (optind == argc) {
+        // 파일 인자 없으면 stdin 읽기
+        int line = 1;
+        char buffer[1024];
+        while (fgets(buffer, sizeof(buffer), stdin)) {
+            if (show_line_numbers)
+                printf("%6d\t", line++);
+            printf("%s", buffer);
+            if (show_ends)
+                printf("$");
+        }
+        return 0;
+    }
+
+    // 파일 인자 처리
+    for (int i = optind; i < argc; i++) {
+        FILE *fp = fopen(argv[i], "r");
+        if (!fp) {
+            perror(argv[i]);
+            continue;
+        }
+        int line = 1;
+        char buffer[1024];
+        while (fgets(buffer, sizeof(buffer), fp)) {
+            if (show_line_numbers)
+                printf("%6d\t", line++);
+            printf("%s", buffer);
+            if (show_ends)
+                printf("$");
+        }
+        fclose(fp);
+    }
+
+    return 0;
 }
