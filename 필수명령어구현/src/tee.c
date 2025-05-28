f7e3b77139dc87f093b57646d0b46004353be8b9
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 17:00:13 +0900

    Update tee.c

--- 필수명령어구현/src/tee.c
+++ 필수명령어구현/src/tee.c
@@ -1,11 +1,42 @@
-// 27. tee.c
 #include <stdio.h>
-// 표준출력과 파일 동시 저장
+#include <stdlib.h>
+#include <getopt.h>
+
+// tee 명령어: 표준 출력을 파일에 동시에 저장
+// 옵션: -a (파일에 추가), 기본은 덮어쓰기
 int main(int argc, char *argv[]) {
-    if (argc != 2) { printf("Usage: %s <file>\n", argv[0]); return 1; }
-    FILE *fp = fopen(argv[1], "w"); if (!fp) { perror("fopen"); return 1; }
-    char line[1024]; while (fgets(line, sizeof(line), stdin)) {
-        fputs(line, stdout); fputs(line, fp);
+    int opt;
+    int append = 0;
+
+    while ((opt = getopt(argc, argv, "a")) != -1) {
+        switch (opt) {
+            case 'a':
+                append = 1;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-a] file\n", argv[0]);
+                return 1;
+        }
     }
-    fclose(fp); return 0;
+
+    if (optind >= argc) {
+        fprintf(stderr, "Usage: %s [-a] file\n", argv[0]);
+        return 1;
+    }
+
+    const char *filename = argv[optind];
+    FILE *fp = fopen(filename, append ? "a" : "w");
+    if (!fp) {
+        perror("fopen");
+        return 1;
+    }
+
+    char line[1024];
+    while (fgets(line, sizeof(line), stdin)) {
+        fputs(line, stdout);
+        fputs(line, fp);
+    }
+
+    fclose(fp);
+    return 0;
 }
