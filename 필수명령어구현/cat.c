2a9401b3b54934c2efc9b9316e410b1a40b371cb
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 15:33:47 +0900

    Delete 필수명령어구현/cat.c

--- 필수명령어구현/cat.c
+++
@@ -1,8 +0,0 @@
-// 5. cat.c
-#include <stdio.h>
-// 파일 내용 출력
-int main(int argc, char *argv[]) {
-    if (argc != 2) { printf("Usage: %s <file>\n", argv[0]); return 1; }
-    FILE *fp = fopen(argv[1], "r"); if (!fp) { perror("fopen"); return 1; }
-    int c; while ((c = fgetc(fp)) != EOF) putchar(c); fclose(fp); return 0;
-}
