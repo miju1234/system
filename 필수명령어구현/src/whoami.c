93d4555c4c2e6e5dd06820d9828049f1de0a0b38
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 17:05:15 +0900

    Update whoami.c

--- 필수명령어구현/src/whoami.c
+++ 필수명령어구현/src/whoami.c
@@ -1,5 +1,24 @@
-// 32. whoami.c
 #include <stdio.h>
 #include <unistd.h>
-// 로그인 사용자 출력
-int main() { printf("%s\n", getlogin()); return 0; }
+#include <getopt.h>
+
+// whoami 명령어: 현재 로그인한 사용자 이름 출력
+// 옵션: 없음 (기본 동작만 수행)
+int main(int argc, char *argv[]) {
+    int opt;
+
+    while ((opt = getopt(argc, argv, "")) != -1) {
+        fprintf(stderr, "Usage: %s\n", argv[0]);
+        return 1;
+    }
+
+    char *username = getlogin();
+    if (username) {
+        printf("%s\n", username);
+    } else {
+        perror("getlogin");
+        return 1;
+    }
+
+    return 0;
+}
