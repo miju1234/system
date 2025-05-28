083ff4c48e16687b4ab599868e5e03baa3a3b1aa
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 15:34:13 +0900

    Delete 필수명령어구현/mkdir.c

--- 필수명령어구현/mkdir.c
+++
@@ -1,8 +0,0 @@
-// 3. mkdir.c
-#include <stdio.h>
-#include <sys/stat.h>
-// 디렉토리 생성
-int main(int argc, char *argv[]) {
-    if (argc != 2) { printf("Usage: %s <dir>\n", argv[0]); return 1; }
-    if (mkdir(argv[1], 0755) != 0) perror("mkdir"); return 0;
-}
