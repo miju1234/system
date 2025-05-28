08490117f4de61b76d70c5d112f17d6a7a509a6c
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 16:02:55 +0900

    Create less.c

---
+++ 필수명령어구현/src/less.c
@@ -0,0 +1,12 @@
+// 26. less.c
+#include <stdio.h>
+// 페이지 출력 및 이전 페이지 가능 (간단)
+int main(int argc, char *argv[]) {
+    if (argc != 2) { printf("Usage: %s <file>\n", argv[0]); return 1; }
+    FILE *fp = fopen(argv[1], "r"); if (!fp) { perror("fopen"); return 1; }
+    char line[1024]; int count=0;
+    while (fgets(line, sizeof(line), fp)) {
+        printf("%s", line); if (++count%20==0) { printf("--Less--"); getchar(); }
+    }
+    fclose(fp); return 0;
+}
