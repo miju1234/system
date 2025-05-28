a7432f4bbc8bf6912f08682a7b4e7cf18886844a
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 16:39:18 +0900

    Update clear.c

--- 필수명령어구현/src/clear.c
+++ 필수명령어구현/src/clear.c
@@ -1,4 +1,30 @@
-// 44. clear.c
 #include <stdio.h>
-// 터미널 클리어
-int main() { printf("\033[H\033[J"); return 0; }
+#include <getopt.h>
+
+// clear 명령어: 터미널 화면 지우기
+// 옵션: -x (디버그용, clear 동작 대신 메시지 출력)
+int main(int argc, char *argv[]) {
+    int opt;
+    int debug = 0;
+
+    // 옵션 파싱
+    while ((opt = getopt(argc, argv, "x")) != -1) {
+        switch (opt) {
+            case 'x':
+                debug = 1;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-x]\n", argv[0]);
+                return 1;
+        }
+    }
+
+    if (debug) {
+        printf("DEBUG: Clear screen (simulated)\n");
+    } else {
+        // ANSI 이스케이프 시퀀스로 화면 지우기
+        printf("\033[H\033[J");
+    }
+
+    return 0;
+}
