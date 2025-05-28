8ac445369c1cdbc3a8962685d99a5357aa12124e
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 16:47:45 +0900

    Update hostname.c

--- 필수명령어구현/src/hostname.c
+++ 필수명령어구현/src/hostname.c
@@ -1,7 +1,44 @@
-// 35. hostname.c
 #include <stdio.h>
 #include <unistd.h>
-// 호스트 이름 출력
-int main() {
-    char host[256]; gethostname(host,sizeof(host)); printf("%s\n",host); return 0;
+#include <getopt.h>
+
+// hostname 명령어: 시스템 호스트 이름 출력/설정
+// 옵션: -s (짧은 이름 출력), -h (호스트 이름 설정)
+int main(int argc, char *argv[]) {
+    int opt;
+    int short_name = 0;
+    char *new_name = NULL;
+
+    // 옵션 파싱
+    while ((opt = getopt(argc, argv, "sh:")) != -1) {
+        switch (opt) {
+            case 's': short_name = 1; break;
+            case 'h': new_name = optarg; break;
+            default:
+                fprintf(stderr, "Usage: %s [-s] [-h name]\n", argv[0]);
+                return 1;
+        }
+    }
+
+    char hostname[256];
+
+    if (new_name) {
+        if (sethostname(new_name, strlen(new_name)) != 0) {
+            perror("sethostname");
+            return 1;
+        }
+        printf("Hostname set to %s\n", new_name);
+    } else {
+        if (gethostname(hostname, sizeof(hostname)) != 0) {
+            perror("gethostname");
+            return 1;
+        }
+        if (short_name) {
+            char *dot = strchr(hostname, '.');
+            if (dot) *dot = '\0';
+        }
+        printf("%s\n", hostname);
+    }
+
+    return 0;
 }
