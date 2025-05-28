053b02667f5e6428f15e5ec06521db8b4b7b98e6
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 17:05:51 +0900

    Update yes.c

--- 필수명령어구현/src/yes.c
+++ 필수명령어구현/src/yes.c
@@ -1,4 +1,24 @@
-// 45. yes.c
 #include <stdio.h>
-// y 반복 출력
-int main() { while(1) printf("y\n"); return 0; }
+#include <getopt.h>
+
+// yes 명령어: "y" 또는 지정한 문자열을 무한 반복 출력
+// 옵션: 없음 (기본 동작만 수행)
+int main(int argc, char *argv[]) {
+    int opt;
+
+    while ((opt = getopt(argc, argv, "")) != -1) {
+        fprintf(stderr, "Usage: %s [string]\n", argv[0]);
+        return 1;
+    }
+
+    const char *str = "y";
+    if (optind < argc) {
+        str = argv[optind];
+    }
+
+    while (1) {
+        printf("%s\n", str);
+    }
+
+    return 0;
+}
