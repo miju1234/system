7eb5997b3c36651bb2819e5b8a9305ae2da63bea
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 16:43:48 +0900

    Update env.c

--- 필수명령어구현/src/env.c
+++ 필수명령어구현/src/env.c
@@ -1,6 +1,46 @@
-// 41. env.c
 #include <stdio.h>
 #include <stdlib.h>
-// 환경 변수 출력
+#include <string.h>
+#include <getopt.h>
+
+// env 명령어: 환경 변수 출력
+// 옵션: -u (특정 변수 제거), -i (빈 환경에서 시작)
 extern char **environ;
-int main() { for (char **e=environ; *e; e++) printf("%s\n", *e); return 0; }
+
+int main(int argc, char *argv[]) {
+    int opt;
+    char *unset_var = NULL;
+    int ignore_env = 0;
+
+    // 옵션 파싱
+    while ((opt = getopt(argc, argv, "u:i")) != -1) {
+        switch (opt) {
+            case 'u':
+                unset_var = optarg;
+                break;
+            case 'i':
+                ignore_env = 1;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-u var] [-i]\n", argv[0]);
+                return 1;
+        }
+    }
+
+    if (ignore_env) {
+        // 빈 환경에서 시작
+        environ = NULL;
+    } else if (unset_var) {
+        // 특정 변수 제거
+        unsetenv(unset_var);
+    }
+
+    // 환경 변수 출력
+    if (environ) {
+        for (char **env = environ; *env; env++) {
+            printf("%s\n", *env);
+        }
+    }
+
+    return 0;
+}
