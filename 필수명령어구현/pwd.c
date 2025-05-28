018931f1fae7ce4580c93e67afe1dd375281ded5
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 15:34:22 +0900

    Delete 필수명령어구현/pwd.c

--- 필수명령어구현/pwd.c
+++
@@ -1,9 +0,0 @@
-// 2. pwd.c
-#include <stdio.h>
-#include <unistd.h>
-#include <limits.h>
-// 현재 경로 출력
-int main() {
-    char cwd[PATH_MAX]; if (getcwd(cwd, sizeof(cwd))) printf("%s\n", cwd);
-    else perror("getcwd"); return 0;
-}
