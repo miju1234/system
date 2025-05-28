b0c6a8403206fed7eab511a02528fd396d4ebff7
miju1234 <118785885+miju1234@users.noreply.github.com>
2025-05-28 16:37:55 +0900

    Update chmod.c

--- 필수명령어구현/src/chmod.c
+++ 필수명령어구현/src/chmod.c
@@ -1,9 +1,46 @@
-// 11. chmod.c
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
-// 권한 변경
+#include <getopt.h>
+
+// chmod 명령어: 파일 권한 변경
+// 옵션: -v (verbose, 변경된 권한 출력)
 int main(int argc, char *argv[]) {
-    if (argc != 3) { printf("Usage: %s <file> <mode(octal)>\n", argv[0]); return 1; }
-    mode_t mode = strtol(argv[2], NULL, 8); if (chmod(argv[1], mode) != 0) perror("chmod");
+    int opt;
+    int verbose = 0;
+
+    // 옵션 파싱
+    while ((opt = getopt(argc, argv, "v")) != -1) {
+        switch (opt) {
+            case 'v':
+                verbose = 1;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-v] mode file...\n", argv[0]);
+                return 1;
+        }
+    }
+
+    if (optind + 1 >= argc) {
+        fprintf(stderr, "Usage: %s [-v] mode file...\n", argv[0]);
+        return 1;
+    }
+
+    // 모드 파싱
+    mode_t mode = strtol(argv[optind], NULL, 8);
+    if (mode == 0 && argv[optind][0] != '0') {
+        perror("Invalid mode");
+        return 1;
+    }
+
+    // 파일들 처리
+    for (int i = optind + 1; i < argc; i++) {
+        if (chmod(argv[i], mode) != 0) {
+            perror(argv[i]);
+        } else if (verbose) {
+            printf("mode of '%s' changed to %o\n", argv[i], mode);
+        }
+    }
+
     return 0;
 }
